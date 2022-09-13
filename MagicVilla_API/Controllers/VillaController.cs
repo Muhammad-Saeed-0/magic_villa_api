896f9c4f6f53351887619bba991b370d519b129d
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2022-09-13 15:45:32 +0200

    first endpoint and DTO

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla_API.Models;
+using MagicVilla_API.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/Villa")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>
+            { 
+                new VillaDTO{Id = 1, Name = "Villa1" },
+                new VillaDTO{Id = 2, Name="Villa2"}
+
+            };
+        }
+    }
+}
